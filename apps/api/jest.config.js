2b680a84c983feb7193e75cdd2cc6771fe8cc747
golfergeek <golfergeet@gmail.com>
2025-05-12 09:40:15 -0500

    chore: moved files

--- apps/api/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
-  testEnvironment: "node",
-  transform: {
-    "^.+\.tsx?$": ["ts-jest",{}],
-  },
-  testMatch: ["<rootDir>/src/test/**/*.test.ts"],
-  moduleFileExtensions: ["ts", "js", "json", "node"],
-};
